da6affd18e57ebb2fe1cfa823d13f3a1ffb38b71
Thomas Couvreur <t.couvreurthomas@gmail.com>
2021-01-08 16:08:35 +0100

    first middleware

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ const helmet = require("helmet");
 const app = express();
 require("dotenv").config();
 
+module.exports = app;
+
 global.render=require("./backend/controllers/render");
-global.bodyParser = require("body-parser");
+// global.bodyParser = require("body-parser");
 
 app.use(helmet());
 
 // app.use(bodyParser.json());
 
-// app.post('/api/stuff',  (req, res) => {
-//   console.log(req.body);
-//   res.status(201).json({
-//     message: 'Objet créé ! :)'
-//   });
-// });
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next;
+});
+
+app.use((req, res, next) => {
+  res.json({
+    message: 'Objet créé ! :)'
+  });
+});
 
 const sauces = require("./backend/routes/sauces");
 app.use("/api", sauces);
 
-app.listen(process.env.PORT || 3001);
-console.log("listen");
 // process.env.SECRET_KEY
